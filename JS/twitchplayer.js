3f12951f80f7979f43c51e2c8af117d99db6b499
VitcorLF <victor-lf@outlook.com>
2020-05-07 16:55:32 -0300

    Edit

---
+++ JS/twitchplayer.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+document.write( '    <script type=\"text/javascript\">\n' );
+document.write( '      new Twitch.Embed(\"twitch-embed\", {\n' );
+document.write( '	width: \"100%\",\n' );
+document.write( '	height: 480,\n' );
+document.write( '	channel: \"VitcorLF\",\n' );
+document.write( '	theme: \"Dark\",\n' );
+document.write( '      });\n' );
+document.write( '    </script>' );
